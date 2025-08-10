5d0f5df7d14f225efb0ae74ab963c2cac1fa2334
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-10 13:08:25 +0530

    fix classname

--- src/components/ContentGrid.tsx
+++ src/components/ContentGrid.tsx
@@ -1,14 +1,16 @@
+import clsx from "clsx";
 import type { ReactNode } from "react"
 
 interface IProp {
     children: ReactNode;
     gridTitle: string;
+    className?: string;
 }
 
-function ContentGrid({ children, gridTitle }: IProp) {
+function ContentGrid({ children, gridTitle, className }: IProp) {
     return (
-        <div className="pt-16">
-            <h1 className="text-2xl font-semibold font-rubik text-zinc-900 capitalize">
+        <div className={clsx(className || "pt-16")}>
+            <h1 className="text-3xl font-semibold font-rubik text-zinc-900 capitalize">
                 {gridTitle}
             </h1>
             <div className="grid grid-cols-8 mt-5 gap-2">
