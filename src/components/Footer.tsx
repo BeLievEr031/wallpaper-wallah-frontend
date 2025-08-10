bc5f88a81bbbe8a3f48e32120442f48ebd57c7ed
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-10 15:48:27 +0530

    fix:add border top

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,28 @@
 import CategoryAndSubCategory from "./CategoryAndSubCategory"
+import Container from "./Container"
 
 function Footer() {
     return (
-        <div className="pt-18 font-rubik">
-            <h1 className="text-3xl font-semibold font-rubik text-zinc-900 capitalize">Explore other wallpaper categories</h1>
+        <div className="mt-16 pt-2 font-rubik border-t-2">
+            <Container>
+                <h1 className="text-3xl font-semibold font-rubik text-zinc-900 capitalize">Explore other wallpaper categories</h1>
 
-            <div className="grid grid-cols-5 gap-x-6">
-                <CategoryAndSubCategory
-                    name="Animal"
-                    subcategory={[
-                        "Kuromi",
-                        "Cinnamoroll",
-                        "Itachi",
-                        "Sanrio",
-                        "My melody",
-                        "Luffy gear 5",
-                        "Alastor",
-                    ]}
-                />
+                <div className="grid grid-cols-5 gap-x-6">
+                    <CategoryAndSubCategory
+                        name="Animal"
+                        subcategory={[
+                            "Kuromi",
+                            "Cinnamoroll",
+                            "Itachi",
+                            "Sanrio",
+                            "My melody",
+                            "Luffy gear 5",
+                            "Alastor",
+                        ]}
+                    />
 
-            </div>
+                </div>
+            </Container>
         </div>
     )
 }
