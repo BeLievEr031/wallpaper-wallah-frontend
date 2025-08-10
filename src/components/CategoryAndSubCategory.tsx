d43e16f236f56fcc01ea2515a3f5b056c1c0b1b5
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-10 15:26:22 +0530

    fix: mx space

--- src/components/CategoryAndSubCategory.tsx
+++ src/components/CategoryAndSubCategory.tsx
@@ -7,7 +7,7 @@ function CategoryAndSubCategory({ name, subcategory = [] }: ICategory) {
             <h1 className="text-lg font-semibold">{name}</h1>
             <ul className="flex flex-wrap items-center gap-x-0.5 text-lg font-medium text-gray-700 list-none p-0 m-0">
                 {subcategory.map((item, index, arr) => (
-                    <li key={index} className="flex items-center">
+                    <li key={index} className="flex items-center mx-0.5">
                         {index !== 0 && <span className="mr-0.5 text-xs">·</span>}
                         <span className="hover:text-blue-600 transition-colors duration-200 cursor-pointer text-sm">
                             {item}
