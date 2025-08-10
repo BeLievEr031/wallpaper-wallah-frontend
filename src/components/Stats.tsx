3571c0664f1189f3bff30a7889f1243255caeadf
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-10 13:08:15 +0530

    feat: add stat section

---
+++ src/components/Stats.tsx
@@ -0,0 +1,24 @@
+
+const Stats = () => {
+    const stats = [
+        { label: "Wallpapers Available", value: "25,430+" },
+        { label: "Categories", value: "50+" },
+        { label: "Active Users", value: "120k+" },
+        { label: "Total Downloads", value: "2M+" },
+    ];
+
+    return (
+        <section className="my-20 font-rubik">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
+                {stats.map((stat, idx) => (
+                    <div key={idx} className="">
+                        <h2 className="text-6xl font-bold text-blue-600">{stat.value}</h2>
+                        <p className="mt-2 text-gray-700 font-semibold text-3xl">{stat.label}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Stats;
