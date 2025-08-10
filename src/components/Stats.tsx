9c81d6b4a10a71ff8cba1ce7586170017fe05e64
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-10 15:48:15 +0530

    fix: add border left

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 
 const Stats = () => {
     const stats = [
@@ -11,13 +12,13 @@ const Stats = () => {
         <section className="my-20 font-rubik">
             <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
                 {stats.map((stat, idx) => (
-                    <div key={idx} className="">
+                    <div key={idx} className={clsx(idx !== 0 && "border-l-2 border-zinc-600")}>
                         <h2 className="text-6xl font-bold text-blue-600">{stat.value}</h2>
                         <p className="mt-2 text-gray-700 font-semibold text-3xl">{stat.label}</p>
                     </div>
                 ))}
             </div>
-        </section>
+        </section >
     );
 };
 
