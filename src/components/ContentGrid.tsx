d60b1029fbd2ee66d77fc3e14d3e4bfb17d5a257
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-11 22:49:51 +0530

    feat:add content grid responsive

--- src/components/ContentGrid.tsx
+++ src/components/ContentGrid.tsx
@@ -10,10 +10,10 @@ interface IProp {
 function ContentGrid({ children, gridTitle, className }: IProp) {
     return (
         <div className={clsx(className || "pt-16")}>
-            <h1 className="text-3xl font-semibold font-rubik text-zinc-900 capitalize">
+            <h1 className="text-xl md:text-3xl font-semibold font-rubik text-zinc-900 capitalize">
                 {gridTitle}
             </h1>
-            <div className="grid grid-cols-8 mt-5 gap-2">
+            <div className="grid grid-cols-3 md:grid-cols-8 mt-5 gap-2">
                 {children}
             </div>
         </div>
